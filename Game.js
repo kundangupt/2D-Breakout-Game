6b0164249808f5e76c15a241d80a482b8bfe42c2
kundan gupta <kukundan1993@gmail.com>
2023-05-27 22:37:39 +0530

    feature: CheckForCollisions Added

--- Game.js
+++ Game.js
@@ -108,3 +108,32 @@ function moveBall() {
     checkForCollisions()
 }
 timerId = setInterval(moveBall, 30)
+
+//check for collisions
+function checkForCollisions() {
+    //check for block collision
+    for (let i = 0; i < blocks.length; i++){
+      if
+      (
+        (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+        ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]) 
+      )
+        {
+        const allBlocks = Array.from(document.querySelectorAll('.block'))
+        allBlocks[i].classList.remove('block')
+        blocks.splice(i,1)
+        changeDirection()   
+        score++
+        scoreDisplay.innerHTML = score
+        if (blocks.length == 0) {
+          scoreDisplay.innerHTML = 'You Win!'
+          clearInterval(timerId)
+          document.removeEventListener('keydown', moveUser)
+        }
+      }
+    }
+    // check for wall hits
+    if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[0] <= 0 || ballCurrentPosition[1] >= (boardHeight - ballDiameter))
+    {
+      changeDirection()
+    }}
